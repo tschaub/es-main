4982ac6548fc58a161a7f63fe91a737b8b1479c0
Tim Schaub <tim.schaub@gmail.com>
2020-02-19 16:36:34 -0700

    Test extension stripping

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
 import {strictEqual} from 'assert';
-import esMain from './main.js';
-// import testImport from './test-import.js';
+import esMain, {stripExt} from './main.js';
+import testImport from './test-import.js';
 
-// strictEqual(testImport, true, 'import other module');
+strictEqual(testImport, true, 'import other module');
 strictEqual(esMain(import.meta), true, 'run as main');
+
+strictEqual(stripExt('/path/to/file.js'), '/path/to/file');
+strictEqual(stripExt('/path/to/file'), '/path/to/file');
+strictEqual(stripExt('/path.to/file.js'), '/path.to/file');
+strictEqual(stripExt('/path.to/file'), '/path.to/file');
